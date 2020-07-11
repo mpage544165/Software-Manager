a1268a15bc225a958122a7bb1fe8ad4fc264acde
mpage544165 <matthewpage123456789@gmail.com>
2020-07-11 15:58:45 -0700

    Initial commit

---
+++ software-manager/backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to Database
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+// Setup Routes/controllers
+const userRouter = require('./routes/users');
+
+app.use('/users', userRouter);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
