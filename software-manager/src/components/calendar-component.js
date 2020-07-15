de48cb6875ff18b609c0c34d71f01a991233f5ba
mpage544165 <matthewpage123456789@gmail.com>
2020-07-15 15:14:34 -0700

    Complete Calendar Day display logic

--- software-manager/src/components/calendar-component.js
+++ software-manager/src/components/calendar-component.js
@@ -3,49 +3,80 @@ import React, {Component} from 'react';
 //import axios from'axios';
 import CalendarDay from "./calendar-day.component";
 
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 
+                    'July', 'August', 'September', 'October', 'November', 'December'];
+
 const numDaysInMonth = {
-    "jan": 31,
-    "feb": 28,
-    "mar": 31,
-    "apr": 30,
-    "may": 31,
-    "june": 30,
-    "july": 31,
-    "aug": 31,
-    "sept": 30,
-    "oct": 31,
-    "nov": 30,
-    "dec": 31
+    0: 31,
+    1: 28,
+    2: 31,
+    3: 30,
+    4: 31,
+    5: 30,
+    6: 31,
+    7: 31,
+    8: 30,
+    9: 31,
+    10: 30,
+    11: 31
 }
 
 export default class Calendar extends Component {
     constructor(props) {
         super(props);
 
+        this.setCalendarDays = this.setCalendarDays.bind(this);
+
         this.state = {
-            date: new Date(),
+            currentDate: new Date(),
             days: [],
             breakPoint: 4
+        } 
+        
+        
+    }
+
+    componentDidMount() {
+        this.setCalendarDays(this.state.currentDate.getMonth(), this.state.currentDate.getFullYear());
+    }
+
+    setCalendarDays(month, year) {
+        if (month > 11) {
+            month = 0;
+            year++;
+        }
+        else if (month < 0) {
+            month = 11;
+            year--;
         }
 
-        let startDay = new Date(`${this.state.date.getFullYear()}-${this.state.date.getMonth() + 1}-01`).getDay();
+        let startDay = new Date(year, month, 1).getDay();
         console.log(startDay);
+        console.log(Date(year, month, 1));
 
         let nextMonthCounter = 1;
-        let lastMonthCounter = numDaysInMonth['june'];
+        let lastMonthCounter = numDaysInMonth[(month + 11) % 12];
+
+        let newDays = []
         for (let i = 0; i < 42; i++) {
             if(i < startDay) {
-                this.state.days.push(<CalendarDay dayNum={lastMonthCounter - (startDay - i) + 1} key={i}/>)
+                newDays.push(<CalendarDay dayNum={lastMonthCounter - (startDay - i) + 1} key={i}/>)
             }
-            else if (i > (numDaysInMonth['july'] + startDay - 1)){
-                this.state.days.push(<CalendarDay dayNum={nextMonthCounter} key={i}/>)
+            else if (i > (numDaysInMonth[month] + startDay - 1)){
+                newDays.push(<CalendarDay dayNum={nextMonthCounter} key={i}/>)
                 nextMonthCounter++;
             }
             else {
-                this.state.days.push(<CalendarDay dayNum={i - startDay + 1} key={i}/>)
+                newDays.push(<CalendarDay dayNum={i - startDay + 1} key={i}/>)
             }
             
-        }   
+        }
+
+        this.setState({
+            currentDate: new Date(year, month, 1),
+            days: newDays
+        })
+        console.log(this.state.currentDate)
     }
 
     render() {
@@ -53,6 +84,25 @@ export default class Calendar extends Component {
         return (
             <div className="container">
                 <h2>Sprint Calendar</h2>
+
+                <nav aria-label="Page navigation example">
+  <ul className="pagination">
+    <li className="page-item">
+      <a className="page-link" href="#" aria-label="Previous" onClick={() => this.setCalendarDays(this.state.currentDate.getMonth() - 1, this.state.currentDate.getFullYear())}>
+        <span aria-hidden="true">&laquo;</span>
+        <span className="sr-only">Previous</span>
+      </a>
+    </li>
+        <li className="page-item"><a className="page-link" href="#">{monthNames[this.state.currentDate.getMonth()]} {this.state.currentDate.getFullYear()}</a></li>
+    <li className="page-item">
+      <a className="page-link" href="#" aria-label="Next" onClick={() => this.setCalendarDays(this.state.currentDate.getMonth() + 1, this.state.currentDate.getFullYear())}>
+        <span aria-hidden="true">&raquo;</span>
+        <span className="sr-only">Next</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+
                 <div className="row">
                     <div className="col border">Sunday</div>
                     <div className="col border">Monday</div>
